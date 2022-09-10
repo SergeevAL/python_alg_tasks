5253e7c775f9270cee6a31a0027348ce5847cd73
SergeevAL <43037005+SergeevAL@users.noreply.github.com>
2022-09-10 15:27:19 +0300

    Update single_number.py

--- single_number.py
+++ single_number.py
@@ -1,4 +1,6 @@
 # Основной подход сравнивать с помощью xor, 0 ^ 0 = 0, 5^5 = 0, x ^ 0 = x
+# Input: nums = [4,1,2,1,2]
+# Output: 4
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
