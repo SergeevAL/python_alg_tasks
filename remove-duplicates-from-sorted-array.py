1c64021af9130f1b52fa5239543024f54e254a60
SergeevAL <43037005+SergeevAL@users.noreply.github.com>
2022-09-10 11:52:46 +0300

    Update remove-duplicates-from-sorted-array.py

--- remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # индексы начинаются с нуля
+        # current_index который создает новый порядок
+        # на return необходимо current_index + 1 так как последнее значение и идущие за ним не изменяться 
+        current_index = 0
 
+        for i in range(1, len(nums)):
+            if nums[i] > nums[current_index]:
+                current_index += 1
+                nums[current_index] = nums[i]
+        print(nums)
+        return current_index + 1
