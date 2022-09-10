a7c66fa75b490eaf1f7f981707034bce1a6718e3
SergeevAL <43037005+SergeevAL@users.noreply.github.com>
2022-09-10 12:05:45 +0300

    Update remove-duplicates-from-sorted-array.py

--- remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array.py
@@ -4,6 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # Input: nums = [0,0,1,1,1,2,2,3,3,4]
+        # Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+        # Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+        # It does not matter what you leave beyond the returned k (hence they are underscores).
         # индексы начинаются с нуля
         # current_index который создает новый порядок
         # на return необходимо current_index + 1 так как последнее значение и идущие за ним не изменяться 
